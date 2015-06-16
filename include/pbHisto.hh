27293d3fef5b9babf4a247e235e7b17bfc10128a
Joseph Sheldon Jr <jms7085@psu.edu>
2015-06-16 14:36:50 -0400

    Capitalization issue with header file, part 2 of fix

---
+++ include/pbHisto.hh
@@ -0,0 +1,35 @@
+//PbHisto.hh
+//Started 09 June 2015 by J. Sheldon
+//Derived from exrdmHisto.hh, Geant4 example rdecay02
+
+#include "globals.hh"
+#include <vector>
+
+class TFile;
+class TH1D;
+class TNtuple;
+
+class PbHistoMessenger;
+
+class PhHisto
+{
+	public:
+	
+		PbHisto();
+		rmPbHisto();
+		
+		void book(); // Creates empty histogram with set parameters
+		void save(); // Saves to file
+		void set1DHisto(); // Reparameterizes the histogram
+		void fillHisto(G4double, G4double); // Fills histogram with data
+		void fillTuple(G4int, G4double); // Fills tuple with data
+		void addRowToTuple(); //Adds a row to the tuple
+		void setFileName(const G4String& nam); // Sets file name
+		const G4String& getFileName() const;  
+		const G4String& FileType() const;
+	
+	private:
+		
+		
+	
+}
